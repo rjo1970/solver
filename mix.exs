27fa50daa714c1479b527e417420722f853676df
RJ Osborne <rjo1970@gmail.com>
2021-10-09 15:32:50 -0400

    Update package info for Hex.

--- mix.exs
+++ mix.exs
@@ -4,9 +4,12 @@ defmodule Solver.MixProject do
   def project do
     [
       app: :solver,
+      name: "Solver",
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +26,19 @@ defmodule Solver.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "A simple linear function solver.  Define a system of equations, solve them in any direction."
+  end
+
+  defp package() do
+    [
+      name: "Solver",
+      files: ~w(lib priv .formatter.exs license* mix.exs README*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/rjo1970/solver"}
     ]
   end
 end
